e339dcf6c2e1b1a3e1879704d2d96bab1c57ec20
yliubyva <yliubyva@gmail.com>
2024-10-20 19:34:47 +0300

    add background

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,9 @@
+import styles from "./Background.module.css";
+
+export const Background = () => {
+    return (
+        <div className={styles.background}>
+            <div className={styles.circle}></div>
+        </div>
+    )
+}
--- src/layouts/Root/Root.jsx
+++ src/layouts/Root/Root.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../../components/NavBar/NavBar";
+import { Background } from "../../components/Background";
 import styles from "./Root.module.css";
 
 export const Root = () => {
-    return <div>
+    return ( 
+    <div>
+        <Background />
         <NavBar />
         <main>
             <Outlet />
         </main>
     </div>
+    )
 }
