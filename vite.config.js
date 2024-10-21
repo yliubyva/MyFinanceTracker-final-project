f049189e0a80866fa284d028b07e1a79a6f842a4
yliubyva <yliubyva@gmail.com>
2024-10-21 03:17:06 +0300

    add redirects file copying to dist during build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path';
+import { copyFileSync } from 'fs'
 
+copyFileSync(resolve(__dirname, '_redirects'), resolve(__dirname, 'dist/_redirects'));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
